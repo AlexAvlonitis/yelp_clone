78f9657fdc1c8a15aece92bcc445b8c9f2ff2e29
Alex Avlonitis <avlonitis@msn.com>
2016-03-04 12:37:31 +0000

    completed endorsments, WIP: ajax endorsments

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,9 +1,13 @@
 class EndorsementsController < ApplicationController
 
-  def create
-   @review = Review.find(params[:review_id])
-   @review.endorsements.create
-   redirect_to restaurants_path
+  def new
+    @review = Review.find(params[:review_id])
   end
-  
+
+  def create
+    @review = Review.find(params[:review_id])
+    @review.endorsements.create
+    redirect_to restaurants_path
+  end
+
 end
